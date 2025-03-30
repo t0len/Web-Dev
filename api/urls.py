685617dd58c859c29746fd2bc1631de26178306f
T0Len <leelogn3@gmail.com>
2025-03-30 21:26:46 +0500

    lab 8 api

---
+++ api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import ProductList, ProductDetail, CategoryList, CategoryDetail, ProductsByCategory
+
+urlpatterns = [
+    path('products/', ProductList.as_view(), name='product-list'),
+    path('products/<int:pk>/', ProductDetail.as_view(), name='product-detail'),
+    path('categories/', CategoryList.as_view(), name='category-list'),
+    path('categories/<int:pk>/', CategoryDetail.as_view(), name='category-detail'),
+    path('categories/<int:id>/products/', ProductsByCategory.as_view(), name='products-by-category'),
+]
